f6a8b468e7eba0be2d48a08c6ff668cfeaf41f55
YeeeeRQ <YeeeeRQ@outlook.com>
2022-09-13 22:08:33 +0800

    布局更新

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -6,7 +6,7 @@
         <span class="back-rect"></span>
       </span>
       <span class="title">数字藏品</span>
-      <span class="add">上架</span>
+      <span class="collection-add">上架</span>
     </div>
 
     <div class="library-wrapper">
@@ -19,16 +19,35 @@
 
       <div class="library">
         <div class="collections">
-          <div class="collection"></div>
-          <div class="collection"></div>
-          <div class="collection"></div>
+          <div class="collection">
+            <div class="collection-image">
+              <img src="../assets/pic1.png" alt="" />
+              <div class="image-identification"></div>
+            </div>
+
+            <div class="identification">
+              <span class="collection-id">#100</span>
+              <span class="collection-tag">
+                <img :src="myRequire('../../images/2d')" alt="" />
+              </span>
+            </div>
+
+            <div class="operation">
+              <span class="collection-remove">下架</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+//自定义require方法
+const myRequire = (name: string) => {
+  return new URL(`/src/assets/${name}.png`, import.meta.url).href;
+};
+</script>
 
 <style lang="scss" scoped>
 .gallery-wrapper {
@@ -80,7 +99,7 @@
     padding-top: 11px;
     padding-bottom: 11px;
   }
-  .add {
+  .collection-add {
     width: 49px;
     height: 22px;
     background: rgb(61, 151, 255);
@@ -99,7 +118,7 @@
   display: flex;
   flex-direction: column;
   flex: 1;
-  .library-nav {
+  > .library-nav {
     // border: 1px solid gold;
     height: 42px;
     display: flex;
@@ -129,10 +148,68 @@
       }
     }
   }
-
-  .library {
-    // border: 1px solid gray;
+  > .library {
+    // border: 1px solid #fff;
     flex: 1;
+    .collections {
+      height: 100%;
+      display: flex;
+      flex-wrap: wrap;
+      border: 1px solid #fff;
+      .collection {
+        outline: 1px solid #fff;
+        width: 182px;
+        height: 226px;
+        // background: url(10.png);
+        .collection-image {
+          // outline: 1px solid #fff;
+          width: 182px;
+          height: 182px;
+          border-radius: 10%;
+          > img {
+            background-color: antiquewhite;
+            min-width: 100px;
+            min-height: 100px;
+            width: 100%;
+            height: 100%;
+            border-radius: 10%;
+          }
+        }
+        > .identification {
+          height: 23px;
+          display: flex;
+          justify-content: space-between;
+          > .collection-id {
+            font-size: 14px;
+            font-weight: 400;
+            line-height: 18px;
+            text-align: left;
+            vertical-align: center;
+          }
+          > .collection-tag {
+            > img {
+              height: 23px;
+            }
+          }
+        }
+
+        .operation {
+          display: flow-root;
+          > .collection-remove {
+            box-sizing: border-box;
+            float: right;
+            background: rgb(227, 81, 81);
+            border-radius: 4px;
+            margin: 1px 0px 1px 0px;
+            padding: 2px 11px;
+            font-size: 10px;
+            font-weight: 400;
+            line-height: 13px;
+            text-align: center;
+          }
+        }
+      }
+    }
   }
 }
 </style>
