d609059e112c6ef2f39decabf680114a7b91bc7e
YeeeeRQ <YeeeeRQ@outlook.com>
2022-09-13 16:04:33 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Gallery from "./components/Gallery.vue";
+</script>
+
+<template>
+  <!-- <div>Hello</div> -->
+  <Gallery></Gallery>
+</template>
+
+<style scoped></style>
---
+++ src/components/Gallery.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="title">
+    <div class="back"></div>
+    <div class="added"></div>
+  </div>
+
+  <div class="toolbar">
+    <span>藏品</span>
+    <span>头像</span>
+    <span>盲盒</span>
+    <span>其他</span>
+  </div>
+
+  <div class="library">
+    <div class="collections">
+      <div class="collection"></div>
+      <div class="collection"></div>
+      <div class="collection"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped></style>
